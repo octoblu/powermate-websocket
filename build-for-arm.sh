77522041bb5dc52f812fe1b993094a26200486c6
redaphid / royvandewater <a@octoblu.com , r@octoblu.com>
2017-08-10 16:00:00 -0700

    v1.3.0 Debounce differently

---
+++ build-for-arm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# docker run --rm -it --volume $PWD:/work \
+#   --env-file .env \
+#   octoblu/meshblu-connector-builder:linux-armv7 /bin/bash
+
+docker run --rm --volume $PWD:/work \
+  --env-file .env \
+  octoblu/meshblu-connector-builder:linux-armv7 \
+    bash -c 'export HOME=/tmp/cache \
+      && meshblu-connector-pkger \
+      && meshblu-connector-installer-debian --destination-path=/usr/local/bin
+    '
